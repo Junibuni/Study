1724b36f4c3f073c46ee170b5445576da1123de7
Junibuni <rich4103@naver.com>
2023-11-23 00:34:16 +0900

    generator implemented

--- DCGAN/models/models.py
+++ DCGAN/models/models.py
@@ -5,25 +5,35 @@ class Generator(nn.Module):
     def __init__(self, nc, nz, ngf):
         """
         Args:
-            nc = number of channels
+            nc = number of channels of output
             nz = generator input dim
-            ngf = input channel size
+            ngf = feature map channel size (64)
         """
         super().__init__()
+        #input dim size torch.Size([100, 1, 1])
         self.layer1 = nn.Sequential(
-
+            nn.ConvTranspose2d(nz, ngf*8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(ngf*8),
+            nn.ReLU()
         )
         self.layer2 = nn.Sequential(
-
+            nn.ConvTranspose2d(ngf*8, ngf*4, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf*4),
+            nn.ReLU()
         )
         self.layer3 = nn.Sequential(
-
+            nn.ConvTranspose2d(ngf*4, ngf*2, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf*2),
+            nn.ReLU()
         )
         self.layer4 = nn.Sequential(
-
+            nn.ConvTranspose2d(ngf*2, ngf, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU()
         )
         self.last = nn.Sequential(
-
+            nn.ConvTranspose2d(ngf, nc, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh()
         )
 
     def forward(self, x):
