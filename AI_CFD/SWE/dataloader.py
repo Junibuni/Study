789e961d9d34d7ba2a687c65a756cc320eeeed76
Junibuni <rich4103@naver.com>
2024-03-19 16:33:56 +0900

    modify dataloader, add supervised parameters and dataset for linearNet

--- AI_CFD/SWE/dataloader.py
+++ AI_CFD/SWE/dataloader.py
@@ -1,6 +1,7 @@
 #%%
 import os
 import glob
+import re
 
 import torch
 from torch.utils.data import Dataset, DataLoader
@@ -9,10 +10,12 @@ import lightning.pytorch as pl
 import numpy as np
 
 class CustomDataset(Dataset):
-    def __init__(self, root, split="train"):
+    def __init__(self, root, split="train", znum=16):
         super(CustomDataset, self).__init__()
+        self.znum = znum
         self.file_path = os.path.join(root, split)
-        self.files = glob.glob(self.file_path + "/*.asc")
+        files = glob.glob(self.file_path + "/*.asc")
+        self.files = sorted(files, key=self._numerical_sort_key)
 
     def __getitem__(self, index):
         data = np.genfromtxt(self.files[index], skip_header=6)
@@ -26,7 +29,9 @@ class CustomDataset(Dataset):
                                         transforms.Normalize((0.0,), (0.2,))
                                         ])
         img = preprocess(img)
-        return img.float()
+        p = torch.zeros(self.znum)
+        p[-2:] = torch.tensor([1, 1])
+        return img.float(), p.float()
     
     def __len__(self):
         return len(self.files)
@@ -37,13 +42,25 @@ class CustomDataset(Dataset):
         arr = np.mean(arr.reshape(new_x, 2, new_y, 2), axis=(1, 3))
         return arr
 
+    def _get_filename_from_path(self, filepath):
+        return os.path.basename(filepath)
+    
+    def _numerical_sort_key(self, filepath):
+        filename = self._get_filename_from_path(filepath)
+        
+        match = re.search(r'\d+', filename)
+        if match:
+            return int(match.group())
+        else:
+            return filename 
 #%%
 
 class DataModule(pl.LightningDataModule):
-    def __init__(self, *, dataset_root, batch_size):
+    def __init__(self, *, dataset_root, batch_size, shuffle=True):
         super().__init__()
         self.batch_size = batch_size
         self.dataset_root = dataset_root
+        self.shuffle = shuffle
 
     def prepare_data(self):
         CustomDataset(self.dataset_root, "train")
@@ -54,8 +71,56 @@ class DataModule(pl.LightningDataModule):
         self.test_set = CustomDataset(self.dataset_root, "test")
 
     def train_dataloader(self):
-        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=True, num_workers=4, pin_memory=True, persistent_workers=True, drop_last=True)
+        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=4, pin_memory=True, persistent_workers=True, drop_last=True)
     
     def test_dataloader(self):
         return DataLoader(self.test_set, batch_size=self.batch_size, num_workers=4, pin_memory=True, persistent_workers=True, drop_last=True)
 # %%
+    
+class LinearDataSet(Dataset):
+    def __init__(self, root, split="train"):
+        super(LinearDataSet, self).__init__()
+        self.file_path = os.path.join(root, "linear_net")
+        files = glob.glob(self.file_path + "/*.pt")
+        self.files = sorted(files, key=self._numerical_sort_key)
+
+    def __getitem__(self, index):
+        data = torch.load(self.files[index])
+        return data.float()
+    
+    def __len__(self):
+        return len(self.files)
+    
+    def _get_filename_from_path(self, filepath):
+        return os.path.basename(filepath)
+    
+    def _numerical_sort_key(self, filepath):
+        filename = self._get_filename_from_path(filepath)
+        
+        match = re.search(r'\d+', filename)
+        if match:
+            return int(match.group())
+        else:
+            return filename 
+# %%
+
+class LinearDataModule(pl.LightningDataModule):
+    def __init__(self, *, dataset_root, batch_size=30, shuffle=False):
+        super(LinearDataModule, self).__init__()
+        self.batch_size = batch_size
+        self.dataset_root = dataset_root
+        self.shuffle = shuffle
+
+    def prepare_data(self):
+        CustomDataset(self.dataset_root, "train")
+        CustomDataset(self.dataset_root, "test")
+
+    def setup(self, stage):
+        self.train_set = CustomDataset(self.dataset_root, "train")
+        self.test_set = CustomDataset(self.dataset_root, "test")
+
+    def train_dataloader(self):
+        return DataLoader(self.train_set, batch_size=self.batch_size, shuffle=self.shuffle, num_workers=4, pin_memory=True, persistent_workers=True, drop_last=True)
+    
+    def test_dataloader(self):
+        return DataLoader(self.test_set, batch_size=self.batch_size, num_workers=4, pin_memory=True, persistent_workers=True, drop_last=True)
